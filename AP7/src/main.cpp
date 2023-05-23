5ace7656531f9c010672f64b9cfdf9217fca9116
Lorenzo Vagliano <111889654+Lorenzovagliano@users.noreply.github.com>
2023-05-23 12:15:37 -0300

    .

--- AP7/src/main.cpp
+++ AP7/src/main.cpp
@@ -80,6 +80,8 @@ int main(){
     //Etapa 1
     int tamanhos1[10] = {377, 557, 737, 917, 1097, 1277, 1457, 1637, 1817, 2000};
 
+    cout << "ShellSort Etapa 1: " << endl << endl;
+
     long double shellSortTime1 = 0;
     for(int i = 0; i < 10; i++){
         int times = 10 + 10*i;
@@ -95,9 +97,12 @@ int main(){
             double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
             time_taken *= 1e-9;
 
+            cout << "Tamanho: " << tamanhos1[i] << '\t' << "Tempo: " << time_taken << endl;
             shellSortTime1 += time_taken;
         }
     }
+
+    cout << "HeapSort Etapa 1: " << endl << endl;
 
     long double heapSortTime1 = 0;
     for(int i = 0; i < 10; i++){
@@ -114,17 +119,17 @@ int main(){
             double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
             time_taken *= 1e-9;
 
+            cout << "Tamanho: " << tamanhos1[i] << '\t' << "Tempo: " << time_taken << endl;
             heapSortTime1 += time_taken;
         }
     }
 
-    cout << "Etapa 1" << endl;
-    cout << "Shell sort: " << shellSortTime1 << endl;
-    cout << "Heap sort: " << heapSortTime1 << endl;
-
     //Etapa 2
+    cout << "Etapa 2: " << endl << endl;
     cout << "A prómixa etapa demora bastante para executar devido ao tamanho da entradas. ISSO NÃO É UM ERRO, aguarde: " << endl;
     int tamanhos2[10] = {37700, 55700, 73700, 91700, 109700, 127700, 145700, 163700, 181700, 200000};
+
+    cout << "ShellSort Etapa 2: " << endl << endl;
 
     long double shellSortTime2 = 0;
     for(int i = 0; i < 10; i++){
@@ -141,9 +146,12 @@ int main(){
             double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
             time_taken *= 1e-9;
 
+            cout << "Tamanho: " << tamanhos2[i] << '\t' << "Tempo: " << time_taken << endl;
             shellSortTime2 += time_taken;
         }
     }
+
+    cout << "HeapSort Etapa 2: " << endl << endl;
 
     long double heapSortTime2 = 0;
     for(int i = 0; i < 10; i++){
@@ -160,13 +168,18 @@ int main(){
             double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
             time_taken *= 1e-9;
 
+            cout << "Tamanho: " << tamanhos2[i] << '\t' << "Tempo: " << time_taken << endl;
             heapSortTime2 += time_taken;
         }
     }
 
+    cout << "Etapa 1" << endl;
+    cout << "Somatório Shell sort: " << shellSortTime1 << endl;
+    cout << "Somatório Heap sort: " << heapSortTime1 << endl;
+
     cout << "Etapa 2" << endl;
-    cout << "Shell sort: " << shellSortTime2 << endl;
-    cout << "Heap sort: " << heapSortTime2 << endl;
+    cout << "Somatório Shell sort: " << shellSortTime2 << endl;
+    cout << "Somatório Heap sort: " << heapSortTime2 << endl;
 
     return 0;
 }
